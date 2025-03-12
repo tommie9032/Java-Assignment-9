4d719778f82310c83f98b9792174b75b47ad563b
tommie9032 <tomthomas9032@gmail.com>
2025-03-12 11:24:45 +0530

    Forming infinite loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import static java.lang.Thread.sleep;
+
 class MyThread implements Runnable {
+    private int init;
+    private int increment;
+    private int delay;
+    public MyThread(int init, int increment, int delay) {
+        this.init = init;
+        this.increment = increment;
+        this.delay = delay;
+    }
     public void run() {
+        for(;;){
+            System.out.println(init +" ");
+            init += increment;
+            sleep(delay);
+
+        }
         System.out.println("Thread started");
     }
 }
@@ -8,8 +24,8 @@ class MyThread implements Runnable {
 
 class Main{
     public static void main(String[] args) {
-        //MyThread t1 = new MyThread();
-        //t1.start();
-        new Thread(new MyThread()).start();
+        new MyThread(1,3,3000).start();
+        new MyThread(100,50,1000).start();
+
     }
 }
